50108607e9694d2139ecf2b800269427ca9b8319
BonecoJan <boneco.jan@gmail.com>
2022-07-07 19:35:46 +0200

    login

--- Coffe Booking System/Coffe Booking System/Coffe_Booking_SystemApp.swift
+++ Coffe Booking System/Coffe Booking System/Coffe_Booking_SystemApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import JWTDecode
 
 @main
 struct Coffe_Booking_SystemApp: App {
@@ -9,6 +10,7 @@ struct Coffe_Booking_SystemApp: App {
     
     var body: some Scene {
         WindowGroup {
+            //if checkToken() {
             if loginVM.isAuthenticated {
                 MainView().environmentObject(loginVM)
                     .environmentObject(modelService)
@@ -18,5 +20,26 @@ struct Coffe_Booking_SystemApp: App {
                     .environmentObject(modelService)
             }
         }
+    }
+    
+    func checkToken() -> Bool {
+        let tokenID = String(data: KeychainWrapper.standard.get(service: "access-token", account: "Coffe-Booking-System")!, encoding: .utf8)!
+
+        do {
+            let jwt = try decode(jwt: tokenID)
+            if !(jwt.expired) {
+                loginVM.password = String(data: KeychainWrapper.standard.get(service: "password", account: "Coffe-Booking-System")!, encoding: .utf8)!
+                loginVM.id = jwt.claim(name: "id").string!
+                print(tokenID)
+                loginVM.login()
+                return true
+            }
+            else {
+                return false
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        return false
     }
 }
