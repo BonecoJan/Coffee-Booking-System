7880733504061bc6134bf174f4f7f8b94369c6b5
BonecoJan <boneco.jan@gmail.com>
2022-07-19 16:00:35 +0200

    Postman

---
+++ Coffe Booking System/Coffe Booking System/View/SideMenu.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenu.swift
+//  Coffe Booking System
+//
+//  Created by Jan Wasilewitsch on 19.07.22.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenu()
+    }
+}
